4a2eed94e2ed862244a9d4ff596696b9585c76d8
Simonwsy <wangshengyang2004@163.com>
2024-02-11 15:41:47 +0800

    new

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kakiquant",
+    version="0.1.0",
+    author="Shengyang Wang",
+    author_email="shengyang.wang2@dukekunshan.edu.cn",
+    description="A brief description of your project",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Wangshengyang2004/KakiQuant",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "python-dotenv",
+        "lightgbm",
+        "setuptools",
+        "scikit-learn",
+        "pymongo",
+        "catboost",
+        "xgboost",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "openfe",
+        "tqdm",
+        "python-okx",
+        # Note: cudf is handled separately due to CUDA version dependencies
+    ],
+    extras_require={
+        "CUDA11": ["cudf-cu11"],
+        "CUDA12": ["cudf-cu12"],
+    },
+    project_urls={  # Optional
+        "Bug Tracker": "https://github.com/Wangshengyang2004/KakiQuant/issues",
+    },
+)
