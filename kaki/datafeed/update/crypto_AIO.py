60da30ca132182f806f0598aed5bad7fc10c0000
Wangshengyang2004 <wangshengyang2004@163.com>
2024-02-22 16:31:40 +0800

    new

--- kaki/datafeed/update/crypto_AIO.py
+++ kaki/datafeed/update/crypto_AIO.py
@@ -6,18 +6,27 @@ import time
 import concurrent.futures
 import requests
 import okx.PublicData as PublicData
+import okx.MarketData as MarketData
 from pymongo import MongoClient
 from datetime import datetime
 import random
 
 class CryptoDataUpdater:
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
+        self.client = MongoClient('192.168.31.120', 27017)
         self.db = self.client.crypto
         self.collection = self.db.crypto_kline
-        self.base_url = "https://www.okx.com/api/v5/market/history-mark-price-candles"
+        self.base_url = "https://www.okx.com/api/v5/market/history-candles"
         logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", 
                             handlers=[logging.FileHandler("crypto_AIO.log"), logging.StreamHandler()])
+        
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive'
+
+        }
 
     def get_history_candlesticks(self, instId, bar, before=None, after=None, limit=100):
         params = {
@@ -45,10 +54,7 @@ class CryptoDataUpdater:
         publicDataAPI = PublicData.PublicAPI(flag="0")
         spot_result = publicDataAPI.get_instruments(instType="SPOT")
         swap_result = publicDataAPI.get_instruments(instType="SWAP")
-        spot_list = [i["instId"] for i in spot_result["data"]]
-        swap_list = [i["instId"] for i in swap_result["data"]]
-        return spot_list + swap_list
-
+        return [i["instId"] for i in spot_result["data"]] + [i["instId"] for i in swap_result["data"]]
     def process_kline(self, data, inst_id, bar):
         df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm'])
         df['timestamp'] = pd.to_datetime(df['timestamp'].astype(int), unit='ms')
@@ -61,16 +67,14 @@ class CryptoDataUpdater:
 
     def newest_data_ts(self, inst_id, bar):
         result = self.get_history_candlesticks(inst_id, bar, limit=1).json()["data"][0][0]
-        logging.info(f"Newest data for {inst_id}-{bar} is at {self.ts_to_date(result)}.")
+        # logging.info(f"Newest data for {inst_id}-{bar} is at {self.ts_to_date(result)}.")
         return result
 
-    def fetch_kline_data(self, inst_id: str, bar: str, start_date="2019-01-01", initial_delay=1):
-        start_date = pd.to_datetime(start_date)  # Convert start_date to datetime object
-        # Always ensure the newest data is the end_date
-        end_timestamp = self.newest_data_ts(inst_id, bar)
+    def fetch_kline_data(self, inst_id: str, bar: str, initial_delay=1):
+        # end_timestamp = self.newest_data_ts(inst_id, bar)
 
         # Initially, 'before' is None to fetch the latest data
-        a = end_timestamp
+        a = None
         b = None
         is_first_time = True
 
@@ -82,15 +86,15 @@ class CryptoDataUpdater:
 
         while True:
             try:
-                logging.info(f"Fetching data for {inst_id} {bar} from {self.ts_to_date(a)} to {self.ts_to_date(b)}.")
+                # logging.info(f"Fetching data for {inst_id} {bar} from {self.ts_to_date(a)} to {self.ts_to_date(b)}.")
                 params = {
                     'instId': inst_id,
                     'before': str(b) if b else "",
-                    'after': str(a),
+                    'after': str(a) if a else "",
                     'bar': bar
                 }
                 
-                response = requests.get(self.base_url, params=params, timeout=5)
+                response = requests.get(self.base_url, params = params, headers = self.headers, timeout=3)
                 
                 # If http status is not too many request, we can assume that we need to break the loop
                 if response.status_code == 200:
@@ -104,29 +108,29 @@ class CryptoDataUpdater:
 
                         # Process the data
                         df = self.process_kline(result['data'], inst_id=inst_id, bar=bar)
-                        
+                        # print(df)
                         # Insert data to MongoDB if applicable
                         if not df.empty:
                             self.insert_data_to_mongodb(df)
                             logging.info(f"Successfully inserted data for {inst_id} {bar}.")
 
                         # Update 'before' and 'after' for the next request
-                        earliest_timestamp = int(result['data'][-1][0])
+                        a = int(result['data'][-1][0])
                         if is_first_time:
-                            time_interval = int(result['data'][0][0]) - earliest_timestamp
+                            time_interval = int(result['data'][0][0]) - a
                             is_first_time = False
-                        a = earliest_timestamp
                         b = a - time_interval - 4 + random.randint(1, 10)*2
-                        return
+                        continue
+                    
                 elif response.status_code == 429:
                     # Sleep for a random time between 1 and 5 seconds
-                    sleep_time = random.uniform(1, 5)
                     logging.info(f"Too many requests. Sleeping for {sleep_time:.2f} seconds.")
                     time.sleep(sleep_time)
                 
                 else:
                     logging.error(f"Failed to fetch data: {result.text}")
                     break
+
                 # Reduce sleep time after a successful request, but not below the minimum
                 sleep_time = max(min_sleep_time, sleep_time - sleep_adjustment_factor)
 
@@ -148,7 +152,7 @@ class CryptoDataUpdater:
 
     @staticmethod
     def ts_to_date(ts):
-        return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        return datetime.fromtimestamp(ts, datetime).strftime('%Y-%m-%d %H:%M:%S')
     def update_data(self):
         pair_list = self.get_all_coin_pairs()
         # pair_list = ["BTC-USDT-SWAP"]
