d9dc0da2cf6ee712dfd6a52b385d26315cdfef57
Wangshengyang2004 <wangshengyang2004@163.com>
2024-03-05 18:08:33 +0800

    new

--- kaki/datafeed/update/async_crypto.py
+++ kaki/datafeed/update/async_crypto.py
@@ -3,17 +3,18 @@ from datetime import datetime
 import logging
 import asyncio
 import random
-import pymongo
+import aiohttp
 import okx.PublicData as PublicData
 from motor.motor_asyncio import AsyncIOMotorClient
-import time
+import numpy as np
+import typing
+import aiohttp
+
 class AsyncCryptoDataUpdater:
-    def __init__(self):
+    def __init__(self, max_concurrent_requests = 5) -> None:
         self.client = AsyncIOMotorClient('mongodb://localhost:27017')
         self.db = self.client.crypto
-        self.collection = self.db.crypto_kline
-        self.public_data_api = PublicData(flag="0")
-        self.market_url = "https://www.okx.com/api/v5/market/history-mark-price-candles"
+        self.market_url = "https://www.okx.com/api/v5/market/history-candles"
         self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
             "Accept": "application/json",
@@ -25,87 +26,115 @@ class AsyncCryptoDataUpdater:
             "Sec-Fetch-Dest": "empty",
             "Sec-Fetch-Mode": "cors",
         }
-
+        self.session =  None
+        self.semaphore = asyncio.Semaphore(max_concurrent_requests)
         logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", 
-                            handlers=[logging.FileHandler("crypto_AIO.log"), logging.StreamHandler()])
+                            handlers=[logging.FileHandler("aiocrypto.log"), logging.StreamHandler()])
+    async def drop_db(self):
+        await self.client.drop_database('crypto')
+        logging.info("Dropped database 'crypto'.")
+
+    async def start_session(self):
+        if self.session is None or self.session.closed:
+            timeout = aiohttp.ClientTimeout(total=10)
+            self.session = aiohttp.ClientSession(timeout=timeout)
     
-    async def insert_data_to_mongodb(self, data):
+    async def close_session(self):
+        if self.session and not self.session.closed:
+            await self.session.close()
+
+    async def insert_data_to_mongodb(self, collection_name, data: pd.DataFrame) -> None:
         if not data.empty:
+            collection = self.db[collection_name]
             data_dict = data.to_dict('records')
-            await self.collection.insert_many(data_dict)
-            logging.info(f"Inserted {len(data_dict)} new records asynchronously.")
-    
+            await collection.insert_many(data_dict) # type: ignore
+            logging.info(f"Inserted {len(data_dict)} new records into {collection_name} asynchronously.")
+
     async def get_all_coin_pairs(self):
-        publicDataAPI = PublicData.PublicAPI(flag="0")
-        spot_result = publicDataAPI.get_instruments(instType="SPOT")
-        swap_result = publicDataAPI.get_instruments(instType="SWAP")
+        # Wrap the synchronous calls in asyncio.to_thread to run them in separate threads
+        spot_result = await asyncio.to_thread(self._get_spot_instruments)
+        swap_result = await asyncio.to_thread(self._get_swap_instruments)
+        
         spot_list = [i["instId"] for i in spot_result["data"]]
         swap_list = [i["instId"] for i in swap_result["data"]]
         return spot_list + swap_list
+
+    def _get_spot_instruments(self):
+        publicDataAPI = PublicData.PublicAPI(flag="0")
+        return publicDataAPI.get_instruments(instType="SPOT")
+
+    def _get_swap_instruments(self):
+        publicDataAPI = PublicData.PublicAPI(flag="0")
+        return publicDataAPI.get_instruments(instType="SWAP")
+
+    async def now_ts(self, inst_id: str, bar: str) -> np.int64:
+        import datetime
+        now = datetime.datetime.now()
+        return np.int64(now.timestamp() * 1000)
     
-    async def process_kline(self, data, inst_id, bar):
-        df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm'])
-        df['timestamp'] = pd.to_datetime(df['timestamp'].astype(int), unit='ms')
-        numeric_fields = ['open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm']
-        for field in numeric_fields:
-            df[field] = pd.to_numeric(df[field], errors='coerce')
-        df['instId'] = inst_id
-        df['bar'] = bar
-        return df
-    async def fetch_kline_data(self, inst_id: str, bar: str, initial_delay=1):
-        # Automatically fetch all data available, from the very beginning to the present
-        end_timestamp = datetime.now().timestamp()
+
+    def convert_ints_to_np_int64(self, obj):
+        if isinstance(obj, dict):
+            return {k: self.convert_ints_to_np_int64(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [self.convert_ints_to_np_int64(v) for v in obj]
+        elif isinstance(obj, int):
+            return np.int64(obj)
+        else:
+            return obj
         
-        request_count = 0  # track requests to manage rate limiting
 
-        # Initially, 'before' is set to end_timestamp to fetch the newest data first and move backwards
-        after = str(end_timestamp * 1000)  # API expects milliseconds
-        before = None
+    async def fetch_kline_data(self, inst_id: str, bar: str, sleep_time: int = 1, limit: int = 100):
+        
+        latest_ts = await self.now_ts(inst_id, bar)
+        a = latest_ts
+        b : np.int64
+        
         is_first_time = True
+        # Fetch until no more data is returned
         while True:
-            if request_count >= self.requests_per_limit:
-                time.sleep(self.rate_limit_pause)
-                request_count = 0  # reset request count after pausing
-
-            params = {
-                'instId': inst_id,
-                'before': before,
-                'after': after,
-                'bar': bar,
-            }
-            response = await self.http_client.get(self.market_url, params=params, headers=self.headers)
-            if response.status_code == 200:
-                result = response.json()
-                if 'data' in result and result['data']:
-                    fetched_data = result['data']
-                    df = await self.process_kline(fetched_data, inst_id, bar)
-                    await self.insert_data_to_mongodb(df)
+            try:
+                params = {
+                    'instId': inst_id,
+                    'before': "" if is_first_time else str(b),
+                    'after': str(a),
+                    'bar': bar,
+                    'limit': str(limit)
+                }
+                
+                async with self.semaphore:
+                    async with self.session.get(self.market_url, params=params, headers=self.headers) as response:
+                            if response.status == 200:
+                                result = await response.json()
+                                if not result['data']:
+                                    logging.info(f"No more data to fetch or empty data returned for {inst_id}-{bar}.")
+                                    return None
+                                else:
+                                    df = pd.DataFrame(result['data'], columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm'])
+                                    df['timestamp'] = pd.to_datetime(df['timestamp'].astype(np.int64), unit='ms')
+                                    numeric_fields = ['open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm']
+                                    for field in numeric_fields:
+                                        df[field] = pd.to_numeric(df[field], errors='coerce')
+                                    df['instId'] = inst_id
+                                    df['bar'] = bar
+                                    await self.insert_data_to_mongodb(f"kline-{bar}", df)  # Adjust as per your actual method signature
+                                    logging.info(f"Successfully inserted data for {inst_id} {bar}.")
+                                    a = np.int64(result['data'][-1][0]) - np.int64(1)
+                                    if is_first_time:
+                                        time_interval = abs(np.int64(result['data'][0][0]) - a)
+                                        is_first_time = False
+                                    b = a - time_interval - np.int64(4) + np.int64(random.randint(1, 10)*2)
                     
-                    # Update 'before' to the earliest timestamp in the fetched data to get older data in next request
-                    earliest_timestamp = int(result['data'][-1][0])
-                    if is_first_time:
-                        time_interval = int(result['data'][0][0]) - earliest_timestamp
-                        is_first_time = False
-                    after = earliest_timestamp
-                    before = after - time_interval - 4 + random.randint(1, 10)*2
-                    
-                else:
-                    break  # Break the loop if no data is returned
+                            elif response.status == 429:
+                                logging.info(f"Too many requests for {bar} - {inst_id}.")
+                                # await asyncio.sleep(sleep_time/10)
+                            else:
+                                logging.error(f"Failed to fetch data with status code {response.status}")
+                                return None
             
-            # Too many requests
-            elif response.status_code == 429:
-                # Sleep for a random time between 1 and 5 seconds
-                sleep_time = random.uniform(1, 5)
-                logging.info(f"Too many requests. Sleeping for {sleep_time:.2f} seconds.")
+            except Exception as e:
+                logging.error(f"Error occurred: {e}, Retrying...")
                 await asyncio.sleep(sleep_time)
-            
-            else:
-                print(f"Failed to fetch data: {response.text}")
-                return  # Break the loop on failure
-
-            request_count += 1
-
-        return None
 
     async def initialize_update(self):
         # List of restaurants could be big, think in promise of plying across the sums as detailed.
@@ -119,6 +148,12 @@ class AsyncCryptoDataUpdater:
                 )
         await asyncio.gather(* tasks)
 
+    async def main(self):
+        await self.drop_db()
+        await self.start_session()
+        await self.initialize_update()
+        await self.close_session()
+
 if __name__ == "__main__":
     updater = AsyncCryptoDataUpdater()
-    asyncio.run(updater.initialize_update())
+    asyncio.run(updater.main())
--- kaki/datafeed/update/crypto_AIO.py
+++ kaki/datafeed/update/crypto_AIO.py
@@ -5,10 +5,10 @@ import time
 import concurrent.futures
 import requests
 import okx.PublicData as PublicData
-import okx.MarketData as MarketData
 from pymongo import MongoClient
 import random
 import numpy as np
+from typing import Optional, Union
 
 class CryptoDataUpdater:
     def __init__(self):
@@ -19,12 +19,20 @@ class CryptoDataUpdater:
         logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", 
                             handlers=[logging.FileHandler("crypto_AIO.log"), logging.StreamHandler()])
         
+        self.session = None
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive'
         }
+
+    async def start_session(self):
+        if self.session is None or self.session.closed:
+            self.session = aiohttp.ClientSession()
+
+    def set_mongodb_index(self) -> None:
+        self.collection.create_index([("instId", pymongo.ASCENDING), ("bar", pymongo.ASCENDING), ("timestamp", pymongo.ASCENDING)], unique=True)
 
     def find_bounds(self, inst_id:str, bar:str) -> tuple:
         latest_record = self.collection.find_one({'instId': inst_id, 'bar': bar}, sort=[('timestamp', pymongo.DESCENDING)])
@@ -34,10 +42,18 @@ class CryptoDataUpdater:
             return latest_record['timestamp'], earlist_record['timestamp']
         return None, None
     
-    def post_data_clean(self):
+    def preprocess(self):
+        self.set_mongodb_index()
         pass
 
-    def insert_data_to_mongodb(self, data):
+    def postprocess(self):
+        self.data_clean()
+        pass
+
+    def data_clean(self):
+        pass
+
+    def insert_data_to_mongodb(self, data: pd.DataFrame):
         if not data.empty:
             data_dict = data.to_dict('records')
             self.collection.insert_many(data_dict)
@@ -49,7 +65,7 @@ class CryptoDataUpdater:
         swap_result = publicDataAPI.get_instruments(instType="SWAP")
         return [i["instId"] for i in spot_result["data"]] + [i["instId"] for i in swap_result["data"]]
     
-    def process_kline(self, data, inst_id, bar):
+    def process_kline(self, data: dict, inst_id: str, bar: str) -> pd.DataFrame:
         df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm'])
         df['timestamp'] = pd.to_datetime(df['timestamp'].astype(np.int64), unit='ms')
         numeric_fields = ['open', 'high', 'low', 'close', 'volume', 'volCcy', 'volCcyQuote', 'confirm']
@@ -59,84 +75,94 @@ class CryptoDataUpdater:
         df['bar'] = bar
         return df
 
-    def newest_data_ts(self, inst_id, bar):
-        result = self.get_history_candlesticks(inst_id, bar, limit=1).json()["data"][0][0]
-        # logging.info(f"Newest data for {inst_id}-{bar} is at {self.ts_to_date(result)}.")
+    def newest_data_ts(self, inst_id: str, bar: str) -> np.int64:
+        params = {
+            'instId': inst_id,
+            'bar': bar,
+            'limit': 1
+        }
+        result = self.make_request(params=params).json(parse_int=np.int64)["data"][0][0]
+        logging.info(f"Newest data for {inst_id}-{bar} is at {self.ts_to_date(result)}.")
         return result
 
-    def fetch_kline_data(self, inst_id: str, bar: str, initial_delay=1):
-        # Check DB for existing data
-        db_b, db_a = self.find_bounds(inst_id, bar)
-        # Initially, 'before' is None to fetch the latest data
-        a = None
-        b = None
+    def ts_to_date(self, ts: np.int64) -> str:
+        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(ts) / 1000))
+    
+    def make_request(self, params: dict) -> requests.Response:
+        response = requests.get(self.base_url, params=params, headers=self.headers, timeout=3)
+        return response
+    
+    def convert_ints_to_np_int64(self, obj):
+        if isinstance(obj, dict):
+            return {k: self.convert_ints_to_np_int64(v) for k, v in obj.items()}
+        elif isinstance(obj, list):
+            return [self.convert_ints_to_np_int64(v) for v in obj]
+        elif isinstance(obj, int):
+            return np.int64(obj)
+        else:
+            return obj
+    
+    def fetch_candlesticks(self, inst_id: str, bar: str, before_ts: np.int64, after_ts: np.int64, sleep_time: int = 1, limit: int = 100) -> Optional[pd.DataFrame]:
+        latest_ts = after_ts if after_ts else self.newest_data_ts(inst_id, bar)
+        earliest_ts = before_ts
+        a = latest_ts
+        b = np.int64(a-1)
+        
         is_first_time = True
-
-        # Initialize dynamic sleep time
-        sleep_time = initial_delay
-        min_sleep_time = 0.1  # Minimum sleep time
-        max_sleep_time = 5.0  # Maximum sleep time
-        sleep_adjustment_factor = 0.5  # Factor to adjust sleep time
-
-        while True:
+        while True and b > earliest_ts:
             try:
-                # logging.info(f"Fetching data for {inst_id} {bar} from {self.ts_to_date(a)} to {self.ts_to_date(b)}.")
                 params = {
                     'instId': inst_id,
-                    'before': str(b) if b else "",
-                    'after': str(a) if a else "",
-                    'bar': bar
+                    'before': "" if is_first_time else str(b),
+                    'after': str(a),
+                    'bar': bar,
+                    'limit': limit
                 }
-                
-                response = requests.get(self.base_url, params = params, headers = self.headers, timeout=3)
-                
-                # If http status is not too many request, we can assume that we need to break the loop
+                response = self.make_request(params)
                 if response.status_code == 200:
                     result = response.json(parse_int=np.int64)
-                    logging.info(f"Received {len(result['data'])} records for {inst_id}-{bar}.")
-                    # Check if result is empty or contains data
                     if not result['data']:
                         logging.info(f"No more data to fetch or empty data returned for {inst_id}-{bar}.")
-                        break
+                        return None
                     else:
-
-                        # Process the data
                         df = self.process_kline(result['data'], inst_id=inst_id, bar=bar)
-                        # print(df)
-                        # Insert data to MongoDB if applicable
                         if not df.empty:
                             self.insert_data_to_mongodb(df)
                             logging.info(f"Successfully inserted data for {inst_id} {bar}.")
-
-                        # Update 'before' and 'after' for the next request
                         a = int(result['data'][-1][0])
                         if is_first_time:
                             time_interval = int(result['data'][0][0]) - a
                             is_first_time = False
-                        b = a - time_interval - 4 + random.randint(1, 10)*2
-                        continue
-                    
+                        b = a - time_interval - np.int64(4) + random.randint(1, 10)*2
+                
                 elif response.status_code == 429:
-                    # Sleep for a random time between 1 and 5 seconds
                     logging.info(f"Too many requests. Sleeping for {sleep_time:.2f} seconds.")
                     time.sleep(sleep_time)
                 
                 else:
                     logging.error(f"Failed to fetch data: {result.text}")
-                    break
-
-                # Reduce sleep time after a successful request, but not below the minimum
-                sleep_time = max(min_sleep_time, sleep_time - sleep_adjustment_factor)
-
-                time.sleep(sleep_time)
-
+                    return None
+            
             except Exception as e:
                 logging.error(f"Error occurred: {e}")
-                # Increase sleep time after an error, but not above the maximum
-                sleep_time = min(max_sleep_time, sleep_time + sleep_adjustment_factor)
                 time.sleep(sleep_time)
+    
+    def check_missing_data_to_fill(self, inst_id: str, bar: str) -> Optional[Union[dict, None]]:
+        """
+        Check if there is any missing data for the given inst_id and bar.
+        If there is, return the missing data range in the form of a dict.
+        """
+        pass
 
-    def update_data(self):
+    def fetch_kline_data(self, inst_id: str, bar: str, initial_delay: int = 1) -> None:
+        # Check the missing gap first:
+        missing_dict = self.check_missing_data_to_fill(inst_id, bar)
+        #### NEED TO IMPLEMENT THIS FUNCTION
+
+        # submit the job to concurrent
+ 
+
+    def update_data(self) -> None:
         pair_list = self.get_all_coin_pairs()
         # pair_list = ["BTC-USDT-SWAP"]
         bar_sizes = ['1m', '3m', '5m', '15m', '30m', '1H', '4H', '1D', '1W']
