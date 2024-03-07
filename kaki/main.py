33885d93ef086b7a66af4db9333bef9508bdedd9
simonwsy <wangshengyang2004@163.com>
2024-03-07 10:07:17 +0800

    Changes to be committed: 	modified:   .gitignore 	deleted:    kaki/examples/factor_analysis_v1.9.py 	deleted:    "kaki/examples/\347\254\254\344\272\214\350\256\262\357\274\232Python\346\225\260\346\215\256\346\270\205\346\264\227.ipynb" 	new file:   kaki/factor/ta/quant_calender.py 	modified:   kaki/main.py 	deleted:    kaki/notebooks/high_res_plot.png 	modified:   setup.py

---
+++ kaki/main.py
@@ -0,0 +1,4 @@
+# Maintain DB
+
+# Setup Website
+
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
-
+with open("requirements.txt", "rb") as r:
+    install_requires = r.read().decode("utf-8").split("\n")  
+    
 setup(
     name="kaki",
-    version="0.1.0",
+    version="0.1.1",
     author="Shengyang Wang",
     author_email="shengyang.wang2@dukekunshan.edu.cn",
     description="A brief description of your project",
@@ -16,22 +18,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.10',
-    install_requires=[
-        "python-dotenv",
-        "lightgbm",
-        "setuptools",
-        "scikit-learn",
-        "pymongo",
-        "catboost",
-        "xgboost",
-        "pandas",
-        "matplotlib",
-        "seaborn",
-        "openfe",
-        "tqdm",
-        "python-okx",
-        # Note: cudf is handled separately due to CUDA version dependencies
-    ],
+    install_requires=install_requires,
     extras_require={
         "CUDA11": ["cudf-cu11"],
         "CUDA12": ["cudf-cu12"],
