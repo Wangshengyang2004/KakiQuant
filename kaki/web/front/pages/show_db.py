10a2eab63ed06301be5c95947fbb571e2d0a52a7
Nizar <m1@Nizz.local>
2024-03-24 09:58:42 +0800

    Home page, requirements.txt update, readme update

--- kaki/web/front/pages/show_db.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from pymongo import MongoClient
-from kaki.utils import check_db
-from kaki.utils.check_db import mongodb_general_info
-client = MongoClient(check_db.get_client_str())
-# 设置网页标题
-st.title('DB Stats')
-
-st.json(f'{mongodb_general_info(client)}')
